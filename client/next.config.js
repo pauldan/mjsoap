3ec6cb84a8567c52220701a3524087c19462460b
Paul Dan <paul.dan@gmail.com>
2019-09-26 09:52:48 +0300

    removed basename

--- client/next.config.js
+++ client/next.config.js
@@ -1,6 +1,6 @@
 const webpack = require('webpack');
 
-const { BACKEND_URL = 'http://localhost:7000', BASENAME = '/' } = process.env;
+const { BACKEND_URL = 'http://localhost:7000' } = process.env;
 
 module.exports = {
   webpack: config => {
@@ -12,5 +12,4 @@ module.exports = {
 
     return config;
   },
-  assetPrefix: BASENAME,
 };
